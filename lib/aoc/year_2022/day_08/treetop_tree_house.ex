1a20decf4ec2a71bdd0a12594fbf6470d28baa09
Scott Taylor <staylorwr@users.noreply.github.com>
2022-12-08 08:37:00 -0500

    Day 8, Part 2

--- lib/aoc/year_2022/day_08/treetop_tree_house.ex
+++ lib/aoc/year_2022/day_08/treetop_tree_house.ex
@@ -11,12 +11,15 @@ defmodule Aoc.Year2022.Day08.TreetopTreeHouse do
   def part_2(input) do
     input
     |> Parser.parse()
+    |> State.new()
+    |> Part2.solve()
   end
 
   defmodule Part1 do
+    alias Aoc.Year2022.Day08.TreetopTreeHouse.State
+
     def solve(state) do
-      state.grid
-      |> Enum.count(&tallest_tree?(&1, state))
+      Enum.count(state.grid, &tallest_tree?(&1, state))
     end
 
     def tallest_tree?({{0, _y}, _height}, _state), do: true
@@ -42,33 +45,40 @@ defmodule Aoc.Year2022.Day08.TreetopTreeHouse do
       ~w(north east south west)a
       |> Enum.map(fn dir ->
         dir
-        |> points(point, max)
+        |> State.points(point, max)
         |> Enum.map(fn {x, y} -> grid[{x, y}] end)
         |> Enum.all?(&(&1 < height))
       end)
       |> Enum.any?()
-    end
-
-    def points(:north, {x, y}, _) do
-      Enum.map(0..(y - 1), fn y -> {x, y} end)
-    end
-
-    def points(:east, {x, y}, %{x: max_x}) do
-      Enum.map((x + 1)..max_x, fn x -> {x, y} end)
-    end
-
-    def points(:south, {x, y}, %{y: max_y}) do
-      Enum.map((y + 1)..max_y, fn y -> {x, y} end)
-    end
-
-    def points(:west, {x, y}, _) do
-      Enum.map(0..(x - 1), fn x -> {x, y} end)
     end
   end
 
   defmodule Part2 do
     def solve(state) do
       state
+      |> State.inner_grid()
+      |> Enum.map(fn {point, _height} = grid_point ->
+        [north, east, south, west] =
+          ~w(north east south west)a
+          |> Enum.map(fn dir ->
+            score_for_point(dir, grid_point, state)
+          end)
+
+        {point, north * east * south * west}
+      end)
+      |> Enum.max_by(fn {_point, score} -> score end)
+      |> elem(1)
+    end
+
+    def score_for_point(dir, {point, height}, state) do
+      dir
+      |> State.points(point, state.max)
+      |> Enum.reduce_while(0, fn {x, y}, acc ->
+        case state.grid[{x, y}] do
+          h when h < height -> {:cont, acc + 1}
+          _ -> {:halt, acc + 1}
+        end
+      end)
     end
   end
 
@@ -80,6 +90,28 @@ defmodule Aoc.Year2022.Day08.TreetopTreeHouse do
       x = grid |> Enum.max_by(fn {{x, _}, _} -> x end) |> elem(0) |> elem(0)
       y = grid |> Enum.max_by(fn {{_, y}, _} -> y end) |> elem(0) |> elem(1)
       %__MODULE__{grid: Map.new(grid), max: %{x: x, y: y}}
+    end
+
+    def points(:north, {x, y}, _) do
+      Enum.map((y - 1)..0, fn y -> {x, y} end)
+    end
+
+    def points(:east, {x, y}, %{x: max_x}) do
+      Enum.map((x + 1)..max_x, fn x -> {x, y} end)
+    end
+
+    def points(:south, {x, y}, %{y: max_y}) do
+      Enum.map((y + 1)..max_y, fn y -> {x, y} end)
+    end
+
+    def points(:west, {x, y}, _) do
+      Enum.map((x - 1)..0, fn x -> {x, y} end)
+    end
+
+    def inner_grid(%{grid: grid, max: %{x: max_x, y: max_y}}) do
+      Enum.filter(grid, fn {{x, y}, _} ->
+        x > 0 and x < max_x and y > 0 and y < max_y
+      end)
     end
   end
 
--- test/aoc/year_2022/day_08/treetop_tree_house_test.exs
+++ test/aoc/year_2022/day_08/treetop_tree_house_test.exs
@@ -25,11 +25,34 @@ defmodule Aoc.Year2022.Day08.TreetopTreeHouseTest do
 
   describe "part_2/1" do
     test "examples" do
+      assert Day08.part_2(@example) == 8
     end
 
     @tag day: 08, year: 2022
     test "input", %{input: input} do
-      assert input |> Day08.part_2() == input
+      assert input |> Day08.part_2() == 536_625
+    end
+  end
+
+  describe "Part2.score_for_point" do
+    test "returns values from example" do
+      state = @example |> Day08.Parser.parse() |> Day08.State.new()
+
+      result =
+        ~w(north east south west)a
+        |> Enum.map(fn dir ->
+          {dir, Day08.Part2.score_for_point(dir, {{2, 3}, 5}, state)}
+        end)
+
+      assert [north: 2, east: 2, south: 1, west: 2] == result
+
+      result =
+        ~w(north east south west)a
+        |> Enum.map(fn dir ->
+          {dir, Day08.Part2.score_for_point(dir, {{2, 1}, 5}, state)}
+        end)
+
+      assert [north: 1, east: 2, south: 2, west: 1] == result
     end
   end
 end
